38bf5aa1f47a7932ebf4167d57321d1e1291a8fc
dipaligode <dipaligode2001@gmail.com>
2025-10-28 22:25:24 +0530

    Initial commit - Smart Attendance Next.js setup

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,52 +1,21 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getDatabase } from "firebase/database"; // Import the Firebase Database SDK
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCz-HIZXyFdVQxM0xx_pyKZIqTNnKuGcDE",
-//   authDomain: "smartattendancesystem-10f64.firebaseapp.com",
-//   databaseURL: "https://smartattendancesystem-10f64-default-rtdb.firebaseio.com",
-//   projectId: "smartattendancesystem-10f64",
-//   storageBucket: "smartattendancesystem-10f64.firebasestorage.app",
-//   messagingSenderId: "710704406389",
-//   appId: "1:710704406389:web:f9e13c81d5047b7f5d7d3c",
-//   measurementId: "G-VNTQ550WFH"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// // Initialize Firebase Analytics
-// const analytics = getAnalytics(app);
-
-// // Initialize Firebase Realtime Database
-// const database = getDatabase(app);
-
-// // Export the database so it can be used in other parts of the app
-// export { database, analytics };
-
-
-// Import Firebase SDKs
+// firebase/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCz-HIZXyFdVQxM0xx_pyKZIqTNnKuGcDE",
-  authDomain: "smartattendancesystem-10f64.firebaseapp.com",
-  databaseURL: "https://smartattendancesystem-10f64-default-rtdb.firebaseio.com",
-  projectId: "smartattendancesystem-10f64",
-  storageBucket: "smartattendancesystem-10f64.firebasestorage.app",
-  messagingSenderId: "710704406389",
-  appId: "1:710704406389:web:f9e13c81d5047b7f5d7d3c"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Firebase services
 export const database = getDatabase(app);
 export const auth = getAuth(app);
+export default app;
